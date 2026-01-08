33fffd281a02e8aea08bcdfc4595efdd8793b277
KRISHNA DIPAYAN BHUNIA <krishnabhunia@gmail.com>
2026-01-09 00:45:30 +0530

    Refactor LinkedIn scraping script to improve readability and update search URL

--- linkedin_fetch.py
+++ linkedin_fetch.py
@@ -2,16 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome(service=Service("/path/to/chromedriver"))
+driver = webdriver.Chrome()
+
+# path = '/Users/krishna/Downloads/chromedriver-mac-x64/chromedriver'
+# driver = webdriver.Chrome(service=Service(path))
 driver.get("https://www.linkedin.com/login")
 # → Manually login
 input("Press Enter after login...")
 
-driver.get("https://www.linkedin.com/search/results/people/?geoUrn=[\"107807598\",\"90009654\"]&origin=FACETED_SEARCH&page=2")
+linked_serach_path = "https://www.linkedin.com/search/results/people/?origin=FACETED_SEARCH&page=2&sid=_et"
+driver.get(linked_serach_path)
+print(driver)
 
 names = []
+
+elements = driver.find_elements()
+print(elements)
+
 elements = driver.find_elements(By.CSS_SELECTOR, "span.entity-result__title-text > a > span[aria-hidden='true']")
+print(elements)
+
 for e in elements:
+    print(e.text.strip())
     names.append(e.text.strip())
 
 print(names)
